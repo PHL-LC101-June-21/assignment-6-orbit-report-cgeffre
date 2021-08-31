824f5094a3cfe9813ad2c039684d951340369cdb
cgeffre <cgeffre@gmail.com>
2021-08-30 20:49:46 -0400

    added highlight for space debris

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -13,11 +13,12 @@ export class Satellite {
 		this.orbitType = orbitType;
 		this.operational = operational;
    }
-	
-	isSpaceDebris(): boolean {
+	isSpaceDebris(satelliteType): boolean {
+		if (satelliteType === "Space Debris") {
 		return true;
+		} else
+		return false;
    }
-
 }
 
 // TODO 3a: fix isSpaceDebris check
